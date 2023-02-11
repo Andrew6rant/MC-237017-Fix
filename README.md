54c8f5e7c965244820c0aa1d912e0b1691a558e3
Andrew Grant <andrew.grant@mail.rit.edu>
2023-02-10 21:22:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# MC-237017 FIX
+Minecraft mod to fix [MC-237017](https://bugs.mojang.com/browse/MC-237017) (All caves are filled with lava at and below Y value -54)
+
+This mod is server-side only, and does not require anything on the client. Should work on all versions of Minecraft where this bug is present (1.18+, starting at snapshot 21w37a)
---
+++ src/main/java/io/github/andrew6rant/mc237017fix/mixin/NoiseChunkGeneratorMixin.java
@@ -0,0 +1,19 @@
+package io.github.andrew6rant.mc237017fix.mixin;
+
+import net.minecraft.world.gen.chunk.AquiferSampler;
+import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
+import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin({NoiseChunkGenerator.class})
+public class NoiseChunkGeneratorMixin {
+	/**
+	 * @author Andrew6rant (Andrew Grant)
+	 * @reason Remove the hardcoded -54 lava sea level
+	 */
+	@Overwrite
+	private static AquiferSampler.FluidLevelSampler createFluidLevelSampler(ChunkGeneratorSettings settings) {
+		return (x, y, z) -> new AquiferSampler.FluidLevel(settings.seaLevel(), settings.defaultFluid());
+	}
+}
