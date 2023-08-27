7cb8fce25d1666f6000c983aeb4a8a5d1bd71ede
Andrew Grant <andrew.grant@mail.rit.edu>
2023-08-27 17:48:54 -0400

    port to versions before 1.19.3

--- src/main/java/io/github/andrew6rant/mc237017fix/mixin/NoiseChunkGeneratorMixin.java
+++ src/main/java/io/github/andrew6rant/mc237017fix/mixin/NoiseChunkGeneratorMixin.java
@@ -1,19 +1,30 @@
 package io.github.andrew6rant.mc237017fix.mixin;
 
+import net.minecraft.structure.StructureSet;
+import net.minecraft.util.math.noise.DoublePerlinNoiseSampler;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.chunk.AquiferSampler;
 import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 import net.minecraft.world.gen.chunk.NoiseChunkGenerator;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin({NoiseChunkGenerator.class})
 public class NoiseChunkGeneratorMixin {
-	/**
-	 * @author Andrew6rant (Andrew Grant)
-	 * @reason Remove the hardcoded -54 lava sea level
-	 */
-	@Overwrite
-	private static AquiferSampler.FluidLevelSampler createFluidLevelSampler(ChunkGeneratorSettings settings) {
-		return (x, y, z) -> new AquiferSampler.FluidLevel(settings.seaLevel(), settings.defaultFluid());
+	@Mutable @Shadow @Final private AquiferSampler.FluidLevelSampler fluidLevelSampler;
+
+	@Shadow @Final protected RegistryEntry<ChunkGeneratorSettings> settings;
+
+	@Inject(method = "<init>(Lnet/minecraft/util/registry/Registry;Lnet/minecraft/util/registry/Registry;Lnet/minecraft/world/biome/source/BiomeSource;Lnet/minecraft/util/registry/RegistryEntry;)V", at = @At(value = "TAIL"))
+	private void mc237017fix$redirectFluidLevelSampler(Registry<StructureSet> structureSetRegistry, Registry<DoublePerlinNoiseSampler.NoiseParameters> noiseRegistry, BiomeSource populationSource, RegistryEntry<ChunkGeneratorSettings> chunkGeneratorSettingsRegistryEntry, CallbackInfo ci) {
+		ChunkGeneratorSettings chunkGeneratorSettings = this.settings.value();
+		this.fluidLevelSampler = (x, y, z) -> new AquiferSampler.FluidLevel(chunkGeneratorSettings.seaLevel(), chunkGeneratorSettings.defaultFluid());
 	}
 }
