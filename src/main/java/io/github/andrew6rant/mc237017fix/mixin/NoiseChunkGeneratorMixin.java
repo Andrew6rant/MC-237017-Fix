b0631f210666ac4aa805f7f813ef2ed38986a34e
Andrew Grant <andrew.grant@mail.rit.edu>
2023-08-27 18:46:57 -0400

    simply mixin

--- src/main/java/io/github/andrew6rant/mc237017fix/mixin/NoiseChunkGeneratorMixin.java
+++ src/main/java/io/github/andrew6rant/mc237017fix/mixin/NoiseChunkGeneratorMixin.java
@@ -1,5 +1,6 @@
 package io.github.andrew6rant.mc237017fix.mixin;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.structure.StructureSet;
 import net.minecraft.util.math.noise.DoublePerlinNoiseSampler;
 import net.minecraft.util.registry.Registry;
@@ -12,19 +13,18 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin({NoiseChunkGenerator.class})
 public class NoiseChunkGeneratorMixin {
-	@Mutable @Shadow @Final private AquiferSampler.FluidLevelSampler fluidLevelSampler;
 
 	@Shadow @Final protected RegistryEntry<ChunkGeneratorSettings> settings;
 
-	@Inject(method = "<init>(Lnet/minecraft/util/registry/Registry;Lnet/minecraft/util/registry/Registry;Lnet/minecraft/world/biome/source/BiomeSource;Lnet/minecraft/util/registry/RegistryEntry;)V", at = @At(value = "TAIL"))
-	private void mc237017fix$redirectFluidLevelSampler(Registry<StructureSet> structureSetRegistry, Registry<DoublePerlinNoiseSampler.NoiseParameters> noiseRegistry, BiomeSource populationSource, RegistryEntry<ChunkGeneratorSettings> chunkGeneratorSettingsRegistryEntry, CallbackInfo ci) {
+	@ModifyVariable(method = "<init>",
+	at = @At(value = "STORE"))
+	private AquiferSampler.FluidLevel mc237017fix$modifyLavaFluidLevelSampler(AquiferSampler.FluidLevel fluidLevel) {
 		ChunkGeneratorSettings chunkGeneratorSettings = this.settings.value();
-		this.fluidLevelSampler = (x, y, z) -> new AquiferSampler.FluidLevel(chunkGeneratorSettings.seaLevel(), chunkGeneratorSettings.defaultFluid());
+		return new AquiferSampler.FluidLevel(chunkGeneratorSettings.seaLevel(), chunkGeneratorSettings.defaultFluid());
 	}
 }
